9506ab3512ba0dce02772c349757d4337aae0519
dongpo <dongbianshanpo@163.com>
2020-10-10 22:39:44 +0800

    第一次提交 git-test 工程

---
+++ git-test/src/com/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * @Description TODO
+ * @Author DongPo
+ * @Date 2020/10/10 22:37
+ */
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hell Git");
+	}
+}
